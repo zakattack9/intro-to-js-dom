2e156fe6231a60fcd6e8fd1f193af21c2116c7f4
zakattack9 <zakattack26111@gmail.com>
2017-03-13 08:30:30 -1000

    easy

--- app.js
+++ app.js
@@ -9,3 +9,7 @@ console.log(idElement);
 //console.log(document.getElementsByTagName("div"));
 var tagName = document.getElementsByTagName("div");
 console.log(tagName);
+
+//console.log(document.getElementsByClassName("use-class-method"));
+var className = document.getElementsByClassName("use-class-method");
+console.log(className);
