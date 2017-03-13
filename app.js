e937fa58f26d21f2edfd40fc8affaeaabea03166
zakattack9 <zakattack26111@gmail.com>
2017-03-13 08:28:37 -1000

    easy

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("JS");
+
+//Accesing HTML elements using DOM
+
+//console.log(document.getElementById("use-id-method"));
+var idElement = document.getElementById("use-id-method");
+console.log(idElement);
+
+//console.log(document.getElementsByTagName("div"));
+var tagName = document.getElementsByTagName("div");
+console.log(tagName);
