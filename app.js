2cd81ab5aa2793af60beb41007e472dcc50463b4
zakattack9 <zakattack26111@gmail.com>
2017-03-13 08:44:49 -1000

    easy

--- app.js
+++ app.js
@@ -13,3 +13,10 @@ console.log(tagName);
 //console.log(document.getElementsByClassName("use-class-method"));
 var className = document.getElementsByClassName("use-class-method");
 console.log(className);
+
+//Changing HTML elements using DOM
+var changeId = idElement.innerHTML = "Id has been changed";
+console.log(changeId);
+
+var changeSecondClass = className[1].innerHTML = I've been changed by class name and array location";
+console.log(changeSecondClass);
